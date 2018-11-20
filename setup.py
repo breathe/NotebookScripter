62c441e7391583653dea174b3c6ff0da7ec93c86
N. Ben Cohen <breathevalue@icloud.com>
2018-11-20 16:36:15 +0000

    Fix packaging issue.

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 setup(
     name='NotebookScripter',
-    version='1.0.1',
-    packages=('NotebookScripter',),
+    version='1.0.2',
+    packages='NotebookScripter',
     url='https://github.com/breathe/NotebookScripter',
     license='MIT',
     author='N. Ben Cohen',
     author_email='breathevalue@icloud.com',
     install_requires=(),
     tests_require=(),
-    description=(
-        'Expose ipython jupyter notebooks as callable functions'),
+    description='Expose ipython jupyter notebooks as callable functions.  More info here https://github.com/breathe/NotebookScripter',
+    long_description='Expose ipython jupyter notebooks as callable functions.  More info here https://github.com/breathe/NotebookScripter',
     classifiers=(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
